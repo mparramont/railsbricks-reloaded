82932c357cd3e0f0106f65f540a72d472d6482c4
Miguel Parramon <miguel@ztory.com>
2016-03-12 10:04:29 +0100

    First (rebirth) commit

---
+++ lib/railsbricks/version.rb
@@ -0,0 +1,34 @@
+module Version
+
+  MAJOR = 3
+  MINOR = 2
+  PATCH = 0
+  PRE = nil
+
+  YEAR = "2015"
+  MONTH = "04"
+  DAY = "28"
+
+  def self.to_s
+    [MAJOR, MINOR, PATCH, PRE].compact.join(".")
+  end
+
+  def self.current
+    to_s
+  end
+
+  def self.current_date
+    "#{YEAR}-#{MONTH}-#{DAY}"
+  end
+
+  def self.version_to_h(version)
+    version_array = version.split(/\./)
+    version_hash = {}
+    version_hash[:major] = version_array[0]
+    version_hash[:minor] = version_array[1]
+    version_hash[:patch] = version_array[2]
+    version_hash[:pre] = version_array[3]
+    version_hash
+  end
+
+end
